cc975ddfbe964f71629a37be214a0be3fa2a5ae2
Chino Chang <me@7in0.me>
2019-06-20 14:41:59 +0800

    Add support for PGroonga functions

--- src/PGroonga.EntityFrameworkCore/Query/ExpressionTranslators/Internal/PGroongaMethodTranslator.cs
+++ src/PGroonga.EntityFrameworkCore/Query/ExpressionTranslators/Internal/PGroongaMethodTranslator.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
 using Microsoft.EntityFrameworkCore.Query.ExpressionTranslators;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Query.Expressions.Internal;
 
@@ -11,9 +14,52 @@ namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query.ExpressionTranslators.Inte
     /// </summary>
     public class PGroongaMethodTranslator : IMethodCallTranslator
     {
+        static readonly IReadOnlyDictionary<string, string> SqlNameByMethodName = new Dictionary<string, string>
+        {
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaCommand)] = "pgroonga_command",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaCommandEscapeValue)] = "pgroonga_command_escape_value",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaEscape)] = "pgroonga_escape",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaFlush)] = "pgroonga_flush",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaHighlightHtml)] = "pgroonga_highlight_html",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaIsWritable)] = "pgroonga_is_writable",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaMatchPositionsByte)] = "pgroonga_match_positions_byte",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaMatchPositionsCharacter)] = "pgroonga_match_positions_character",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaNormalize)] = "pgroonga_normalize",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaQueryEscape)] = "pgroonga_query_escape",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaQueryExpand)] = "pgroonga_query_expand",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaQueryExtractKeywords)] = "pgroonga_query_extract_keywords",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaSetWritable)] = "pgroonga_set_writable",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaScore)] = "pgroonga_score",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaSnippetHtml)] = "pgroonga_snippet_html",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaTableName)] = "pgroonga_table_name",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaWalApply)] = "pgroonga_wal_apply",
+            [nameof(PGroongaDbFunctionsExtensions.PgroongaWalTruncate)] = "pgroonga_wal_truncate"
+        };
+
         /// <inheritdoc />
         [CanBeNull]
         public Expression Translate(MethodCallExpression e)
+        {
+            if (e.Method.DeclaringType != typeof(PGroongaDbFunctionsExtensions) &&
+                e.Method.DeclaringType != typeof(PGroongaLinqExtensions))
+                return null;
+
+            if (!SqlNameByMethodName.TryGetValue(e.Method.Name, out var sqlFunctionName))
+                return TryTranslateOperator(e);
+
+            if (sqlFunctionName != "pgroonga_score")
+                return new SqlFunctionExpression(sqlFunctionName, e.Method.ReturnType, e.Arguments.Skip(1));
+
+            // hack for pgroonga_score
+            return new SqlFunctionExpression(sqlFunctionName, e.Method.ReturnType, new[]
+            {
+                new SqlFragmentExpression("tableoid"),
+                new SqlFragmentExpression("ctid")
+            });
+        }
+
+        [CanBeNull]
+        static Expression TryTranslateOperator([NotNull] MethodCallExpression e)
         {
             if (e.Method.DeclaringType != typeof(PGroongaLinqExtensions))
                 return null;
